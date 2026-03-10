75f9582d8e5979dfcf7c4436c07f54bff7476546
enelytr-afk <enelytr@gmail.com>
2026-03-10 09:30:16 -0400

    success

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,6 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
+const authRoutes = require('./routes/authRoutes');
 
 
 const app = express();
@@ -39,6 +40,8 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
